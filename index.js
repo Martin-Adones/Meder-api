fb044dfca7898d481d2d50bb74e463b841c782b1
SepaXC <sebastian.pleticosic@alumnos.ucn.cl>
2025-07-03 15:24:40 -0400

    subir

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
+require('dotenv').config({ path: envFile });
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const path = require('path');
